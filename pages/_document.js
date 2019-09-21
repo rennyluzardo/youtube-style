32d23bf5f12abf48ad2e81e1438ffb9139eea934
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-21 13:13:19 -0500

    added semnatic ui cdn

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,24 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-	render() {
-		return (
-			<html lang="es">
-				<Head>
-					<script>window.dataLayer = window.dataLayer || [];</script>
-					<title>UTube-Style</title>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"></link>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</html>
-		)
-	}
+  render() {
+    return (
+      <html lang="es">
+        <Head>
+          <meta
+            content="width=device-width, initial-scale=1"
+            name="viewport"
+          />
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
 }
