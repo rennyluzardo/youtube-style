8f7a8cebd141eb921e52ac977c120967421016a6
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-20 23:03:54 -0500

    principal components

---
+++ components/global/SearchBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+    render() {
+        return <button className="ui button">Click Here</button>
+    }
+}
+
+export default SearchBar;
---
+++ components/global/VideoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import VideoItem from './VideoItem'
+
+export class VideoList extends Component {
+    render() {
+        return (
+            <div>
+                <VideoItem />
+            </div>
+        );
+    }
+}
+
+export default VideoList;
