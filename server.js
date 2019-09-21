b55b8314735dff2a534388bba568bd327ccff8a3
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-20 23:02:45 -0500

    server port log

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+const port = parseInt(process.env.port, 10) || 3000
 
 // With express
 const express = require('express')
 app.prepare().then(() => {
-  express().use(handler).listen(3000)
+  express().use(handler).listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
