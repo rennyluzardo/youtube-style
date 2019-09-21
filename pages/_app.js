b1e30fc887da00bbdb85cf45c11fb2c3a7df5046
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-20 23:03:38 -0500

    import scss file and babel config

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import App from 'next/app'
 import Layout from '../components/global/Layout'
+import '../scss/styles.scss'
 
 export default class MyApp extends App {
-  render () {
+  render() {
     const { Component, pageProps } = this.props
     return (
       <Layout>
