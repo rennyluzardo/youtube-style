65caed6c277e643da150a86bbbe3cf8c90b000a0
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-22 00:29:19 -0500

    youtube api integration

---
+++ apis/youtube.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export const KEY = 'AIzaSyBO5ew3Dvb63aInBpVswuxkYsFuEDG4GGw'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+})
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const next = require('next')
 const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
-const port = parseInt(process.env.port, 10) || 3000
+const port = process.env.PORT || 3000
 
 // With express
 const express = require('express')
+
 app.prepare().then(() => {
   express().use(handler).listen(port, err => {
     if (err) throw err
