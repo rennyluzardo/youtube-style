a67702631f96261a229412823ef338fb9d1a3417
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-22 00:35:17 -0500

    async term submit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+// Components
 import SearchBar from '../components/global/SearchBar'
+import VideoList from '../components/global/VideoList'
 
-const index = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+import youtube, { KEY } from '../apis/youtube'
+
+export default class index extends Component {
+  state = {
+    videos: []
+  }
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+      }
+    })
+
+    this.setState({ videos: response.data.items })
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList
+          videos={this.state.videos} />
+      </div>
+    )
+  }
 }
-
-export default index;
