575d236092129d18dd2ba9e95d06cefe025d580b
Renny <rennyluzardo.fullstack@gmail.com>
2019-09-22 22:23:17 -0500

    key for video items

--- components/global/VideoList.js
+++ components/global/VideoList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import VideoItem from './VideoItem'
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   return (
     <div className="ui relaxed divided list">
       {
         videos.map(video => {
-          return <VideoItem video={video} />
+          return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
         })
       }
     </div>
